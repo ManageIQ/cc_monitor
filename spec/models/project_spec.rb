bf0b43b0e9fdf88e7b75992c2062207054d42799
Jason Frey <fryguy9@gmail.com>
2013-05-23 17:42:30 -0400

    Cleaned up specs for readability

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -6,7 +6,16 @@ require File.expand_path(File.join(File.dirname(__FILE__), '..', 'spec_helper'))
 
 describe Project do
   before do
-    raw_data = [{:name=>"pg-vmdb_metrics", :status=>:success, :activity=>:building, :url=>"http://cruisecontrol-metrics.manageiq.com:3333/projects/pg-vmdb_metrics", :last_built=>"2013-01-19 03:19:10 -0500", :db=>"pg", :version=>"trunk", :category=>"vmdb_metrics"}]
+    raw_data = [{
+      :name       => "pg-vmdb_metrics",
+      :status     => :success,
+      :activity   => :building,
+      :url        => "http://cruisecontrol-metrics.manageiq.com:3333/projects/pg-vmdb_metrics",
+      :last_built => "2013-01-19 03:19:10 -0500",
+      :db         => "pg",
+      :version    => "trunk",
+      :category   => "vmdb_metrics"
+    }]
     described_class.any_instance.stub(:raw_data => raw_data)
   end
 
@@ -15,6 +24,23 @@ describe Project do
   end
 
   it "#data" do
-    described_class.new.data.should == [["trunk", {"pg"=>{"vmdb_metrics"=>{:name=>"pg-vmdb_metrics", :status=>:success, :activity=>:building, :url=>"http://cruisecontrol-metrics.manageiq.com:3333/projects/pg-vmdb_metrics", :last_built=>"2013-01-19 03:19:10 -0500", :db=>"pg", :version=>"trunk", :category=>"vmdb_metrics"}}}]]
+    described_class.new.data.should == [
+      ["trunk",
+        {
+          "pg" => {
+            "vmdb_metrics" => {
+              :name       => "pg-vmdb_metrics",
+              :status     => :success,
+              :activity   => :building,
+              :url        => "http://cruisecontrol-metrics.manageiq.com:3333/projects/pg-vmdb_metrics",
+              :last_built => "2013-01-19 03:19:10 -0500",
+              :db         => "pg",
+              :version    => "trunk",
+              :category   => "vmdb_metrics"
+            }
+          }
+        }
+      ]
+    ]
   end
 end
