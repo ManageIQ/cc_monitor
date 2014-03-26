5a26e2876a15650ddaf136895cf68df00efc0b44
Brandon Dunne <bdunne@redhat.com>
2014-03-26 11:22:11 -0400

    [cc_monitor] Move row calculation into controller

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -2,6 +2,7 @@ class ProjectController < ApplicationController
   def index
     @categories = Project.categories
     @data       = Project.data(versions)
+    @num_rows   = num_rows
 
     respond_to do |format|
       format.html
@@ -13,5 +14,9 @@ class ProjectController < ApplicationController
 
   def versions
     params['versions'].to_s.split(",") if params['versions']
+  end
+
+  def num_rows
+    @data["versions"].inject(0) { |count, (_, v)| count + v["dbs"].keys.length }
   end
 end
