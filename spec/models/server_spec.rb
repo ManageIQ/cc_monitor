f8151e8584aaee598b6a1011e121e44a261bfbb2
Keenan Brock <kbrock@redhat.com>
2014-06-20 16:27:44 -0400

    write pending server_spec for cc_monitor

--- spec/models/server_spec.rb
+++ spec/models/server_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe Server do
   let(:server) { Server.create!(:url => "http://server/XmlStatusReport.aspx") }
+  let(:project) { Project.create!(:server => server, :name => "pg-migrations") }
 
   it ".refresh" do
     server
@@ -11,29 +12,28 @@ describe Server do
   end
 
   context "#refresh" do
-    pending "Server up" do
+    it "Server up" do
+      xml_data = %(<Projects>
+  <Project name="pg-migrations" category="" activity="CheckingModifications" lastBuildStatus="Success"
+  lastBuildLabel="50505" lastBuildTime="2014-06-20T15:17:36.0000000-0400"
+  nextBuildTime="1970-01-01T00:00:00.000000-00:00" webUrl="http://cc.com/projects/pg-migrations"/>
+</Projects>)
+      expect_any_instance_of(described_class).to receive(:open).and_return(StringIO.new(xml_data))
+      project.server.refresh
+      expect(project.reload.status).to eq("success")
     end
 
-    it "Server down raises error" do
-      server
-      Project.create!(:server_id => server.id)
-
-      described_class.any_instance.should_receive(:open).and_raise(StandardError)
-
-      server.refresh
-
-      expect(Project.first.status).to eq("down")
+    it "should mark server down if server raises error" do
+      expect_any_instance_of(described_class).to receive(:open).and_raise(StandardError)
+      project.server.refresh
+      expect(project.reload.status).to eq("down")
     end
 
-    it "Server down returns 500" do
-      server
-      Project.create!(:server_id => server.id)
-
-      described_class.any_instance.should_receive(:open).and_return("response with 500 Internal Server Error")
-
-      server.refresh
-
-      expect(Project.first.status).to eq("down")
+    it "should mark server down if server returns 500" do
+      xml_data = "<Projects>response with 500 Internal Server Error</Projects>"
+      expect_any_instance_of(described_class).to receive(:open).and_return(StringIO.new(xml_data))
+      project.server.refresh
+      expect(project.reload.status).to eq("down")
     end
   end
 end
